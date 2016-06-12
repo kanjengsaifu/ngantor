bd88c4a6a5b16508e6ce55aba1f0ea65712b5d88
Wirasto S. Karim <wirastokarim@gmail.com>
2016-06-13 04:39:03 +0800

    Seend untuk user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Support\Facades\Hash;
+
+use App\User;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +17,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+        $this->call(UsersTableSeeder::class);
+
+        Model::reguard();
+    }
+}
+
+
+class UsersTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        User::create(array('name'=>'Administrator', 'email'=>'admin@ngantor.com', 'password'=> Hash::make('admin02')));
+        User::create(array('name'=>'Wirasto Karim', 'email'=>'wirasto@ngantor.com', 'password'=> Hash::make('wirasto')));
+        User::create(array('name'=>'Lisa Kawaii', 'email'=>'lisa@ngantor.com', 'password'=> Hash::make('lisa02')));
     }
 }
