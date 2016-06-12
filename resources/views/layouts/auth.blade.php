10850bf49c69a02f5cd7ba67ab0d8089b4c9251b
Wirasto S. Karim <wirastokarim@gmail.com>
2016-06-13 04:59:25 +0800

    Menyediakan layout

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='utf-8' />
+	<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport' />
+
+	<title>Ngantor | Sistem Informasi Tata Usaha</title>
+
+	<!-- Bootstrap 3.3.2 -->
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
+    <!-- FontAwesome 4.3.0 -->
+    <link href="{{ asset('vendor/font-awesome-4.3.0/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css"/>
+    <!-- Theme style -->
+    <link href="{{ asset('vendor/adminLTE/css/AdminLTE.min.css') }}" rel="stylesheet" type="text/css"/>
+</head>
+<body class="login-page">
+
+@yield('content')
+
+</body>
+</html>
---
+++ resources/views/layouts/gui.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+    <title>Ngantor | Sistem Informasi Tata Usaha</title>
+
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport' />
+    <!-- Bootstrap 3.3.2 -->
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
+    <!-- FontAwesome 4.3.0 -->
+    <link href="{{ asset('vendor/font-awesome-4.3.0/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css"/>
+    <!-- Theme style -->
+    <link href="{{ asset('vendor/adminLTE/css/AdminLTE.min.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('vendor/adminLTE/css/skins/_all-skins.min.css') }}" rel="stylesheet" type="text/css"/>
+</head>
+<body class="skin-blue">
+
+<div class="wrapper">
+
+    <header class="main-header">
+        <!-- Logo -->
+        <a href="{{ url() }}" class="logo"><b>Ng</b>antor</a>
+        <!-- Header Navbar: style can be found in header.less -->
+        <nav class="navbar navbar-static-top" role="navigation">
+            <!-- Sidebar toggle button-->
+            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+                <span class="sr-only">Toggle navigation</span>
+            </a>
+            <div class="navbar-custom-menu">
+                <ul class="nav navbar-nav">
+                    <!-- User Account: style can be found in dropdown.less -->
+                    <li class="dropdown user user-menu">
+                        <a href="/" class="dropdown-toggle" data-toggle="dropdown">
+                            <img src="{{ asset('img/photo.jpg') }}" class="user-image" alt="User Image"/>
+                            <span class="hidden-xs">{{ Auth::user()->name }}</span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <!-- User image -->
+                            <li class="user-header">
+                                <img src="{{ asset('img/photo.jpg') }}" class="img-circle" alt="User Image"/>
+                                <p>
+                                   {{ Auth::user()->name }}
+                                    <small>Member since Nov. 2012</small>
+                                </p>
+                            </li>
+                            <!-- Menu Footer-->
+                            <li class="user-footer">
+                                <div class="pull-left">
+                                    <a href="{{ url('me/pwd') }}" class="btn btn-default btn-flat">Ganti Password</a>
+                                </div>
+                                <div class="pull-right">
+                                    <a href="{{ url('logout') }}" class="btn btn-default btn-flat">Logout</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <!-- Left side column. contains the logo and sidebar -->
+    <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+
+            <!-- sidebar menu: : style can be found in sidebar.less -->
+            <ul class="sidebar-menu">
+                <li class="header">MAIN NAVIGATION</li>
+				<li class="{{ Request::is('home') ? 'active' : '' }}"><a href="{{ url('home') }}"><i class="fa fa-dashboard"></i> Beranda</a></li>
+				<li class="{{ Request::is('obat*') ? 'active' : '' }}"><a href="{{ url('obat') }}"><i class="fa fa-medkit"></i> Obat-Obatan</a></li>
+				@can('admin-access')
+				<li class="{{ Request::is('user*') ? 'active' : '' }}"><a href="{{ url('user') }}"><i class="fa fa-group"></i> Operator</a></li>
+                <li class="treeview {{ Request::is('atur*') ? 'active' : '' }}">
+                    <a href="#">
+                        <i class="fa fa-wrench"></i> <span>Pengaturan</span> <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li class="{{ Request::is('atur/kategori*') ? 'active' : '' }}"><a href="{{ url('atur/kategori') }}"><i class="fa fa-circle-o"></i> Kategori Obat</a></li>
+                        <li class="{{ Request::is('atur/satuan*') ? 'active' : '' }}"><a href="{{ url('atur/satuan') }}"><i class="fa fa-circle-o"></i> Satuan Obat</a></li>
+                    </ul>
+                </li>
+				@endcan
+            </ul>
+        </section>
+        <!-- /.sidebar -->
+    </aside>
+
+    <!-- Right side column. Contains the navbar and content of the page -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+				@yield('title', 'Beranda')
+            </h1>
+            <ol class="breadcrumb">
+				@yield('path')
+            </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+		@yield('content')
+
+        </section><!-- /.content -->
+
+    </div><!-- /.content-wrapper -->
+
+    <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+            <b>Version</b> 2.0
+        </div>
+        <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
+        reserved.
+    </footer>
+</div><!-- ./wrapper -->
+
+
+<!-- jQuery 2.1.3 -->
+<script src="{{ asset('vendor/jquery/jQuery-2.1.3.min.js') }}"></script>
+<!-- Bootstrap 3.3.2 JS -->
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<!-- InputMask -->
+<script src="{{ asset('vendor/plugins/input-mask/jquery.inputmask.js') }}" type="text/javascript"></script>
+<script src="{{ asset('vendor/plugins/input-mask/jquery.inputmask.date.extensions.js') }}" type="text/javascript"></script>
+<script src="{{ asset('vendor/plugins/input-mask/jquery.inputmask.extensions.js') }}" type="text/javascript"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('vendor/adminLTE/js/app.js') }}" type="text/javascript"></script>
+@yield('script')
+
+</body>
+</html>
