008e0559e58345ec02c58123a6e638f59b2c56ea
Wirasto S. Karim <wirastokarim@gmail.com>
2016-06-17 06:16:29 +0800

    Menampilkan beberapa agregat di beranda

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Surat\Masuk;
+use App\Models\Surat\Keluar;
 
 class HomeController extends Controller
 {
 	function index()
 	{
-		return view('home');
+		return view('home', [
+				'pegawai_count' => User::count(),
+				'ms_inbox_count' => Masuk::MyInbox()->count(),
+				'ms_masuk_count' => Masuk::count(),
+				'ms_keluar_count' => Keluar::count(),
+			]);
 	}
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,60 @@
 
 @section('content')
 
+<div class="row">
+
+	<div class="col-lg-3 col-xs-6">
+		<div class="small-box bg-yellow">
+			<div class="inner">
+				<h3>{{ $pegawai_count }}</h3>
+				<p>Pegawai</p>
+			</div>
+			<div class="icon">
+				<i class="ion ion-bag"></i>
+			</div>
+			<a href="{{ url('pegawai') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+		</div>
+	</div>
+
+	<div class="col-lg-3 col-xs-6">
+		<div class="small-box bg-aqua">
+			<div class="inner">
+				<h3>{{ $ms_inbox_count }}</h3>
+				<p>Kotak Surat</p>
+			</div>
+			<div class="icon">
+				<i class="ion ion-bag"></i>
+			</div>
+			<a href="{{ url('surat/inbox') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+		</div>
+	</div>
+
+	<div class="col-lg-3 col-xs-6">
+		<div class="small-box bg-green">
+			<div class="inner">
+				<h3>{{ $ms_masuk_count }}</h3>
+				<p>Surat Masuk</p>
+			</div>
+			<div class="icon">
+				<i class="ion ion-bag"></i>
+			</div>
+			<a href="{{ url('surat/masuk') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+		</div>
+	</div>
+
+	<div class="col-lg-3 col-xs-6">
+		<div class="small-box bg-blue">
+			<div class="inner">
+				<h3>{{ $ms_keluar_count }}</h3>
+				<p>Surat Keluar</p>
+			</div>
+			<div class="icon">
+				<i class="ion ion-bag"></i>
+			</div>
+			<a href="{{ url('surat/keluar') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+		</div>
+	</div>
+
+</div>
 
 @endsection
